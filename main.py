9c57f68c782002cd792ba3665194c083504989bb
Madsky39 <skylerbradley1@gmail.com>
2025-09-27 21:13:40 -0700

    now with circles

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pygame
 from constants import *
+
+    
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+
+    dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
     while running == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
         screen.fill(BLACK)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
