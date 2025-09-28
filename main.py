16186c63d3d3f5cd99a36ccef2cf36971619ac37
Madsky39 <skylerbradley1@gmail.com>
2025-09-27 22:00:02 -0700

    now with moving ship

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
     
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    one = Player(x, y)
     dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
@@ -14,7 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill(BLACK)
+        one.draw(screen)
         pygame.display.flip()
+        one.update(dt)
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
